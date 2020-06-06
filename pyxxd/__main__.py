78d31310c809918e4d594e3dd4a907ff2a246b75
Daniil <chay-chitsd@mail.ru>
2020-06-06 12:32:15 +0300

    Add open

--- pyxxd/__main__.py
+++ pyxxd/__main__.py
@@ -1,6 +1,21 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
+FILENAME = ""
+
+def clicked():  
+    Tk().withdraw()
+	FILENAME = askopenfilename()
+
+
+if __name__ == "__main__":
+	window = Frame()
+	window.master.rowconfigure(0, weight=1)
+	window.master.columnconfigure(0, weight=1)
+	window.grid(sticky="NEWS", row=0, column=0)
+	window.rowconfigure(0, weight=1)
+	window.columnconfigure(0, weight=1)
+	btn = Button(window, text="Open", command=clicked)  
+	btn.grid(column=0, row=1, sticky=NEWS)
+	print(FILENAME)
+	mainloop();
