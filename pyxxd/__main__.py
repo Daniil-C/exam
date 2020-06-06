6dd75bd45d7b48c6e9af800879cbbb8eb2b46025
Daniil <chay-chitsd@mail.ru>
2020-06-06 13:29:49 +0300

    Add text functions

--- pyxxd/__main__.py
+++ pyxxd/__main__.py
@@ -2,17 +2,23 @@ import os
 import tkinter
 from tkinter.filedialog import askopenfilename
 
-FILENAME = ""
 
 def openb():  
-    FILENAME = askopenfilename()
-    window.master.title(os.path.basename(FILENAME))
+    filename = askopenfilename()
+    window.master.title(os.path.basename(filename))
     btnsv.config(state = tkinter.NORMAL)
+    text.config(state = tkinter.NORMAL)
+    with open(filename, "r") as fin:
+        src_text = fin.read();
+    text.insert("1.0", src_text)
 
 def saveb():
-    pass
+    new_text = text.get('1.0', 'end')
+    with open(filename, "w") as fout:
+        fout.write(new_text)
 
 if __name__ == "__main__":
+    filename = ""
     window = tkinter.Frame()
     window.master.rowconfigure(0, weight=1)
     window.master.columnconfigure(0, weight=1)
@@ -28,7 +34,7 @@ if __name__ == "__main__":
     btnsv = tkinter.Button(btns, text="Save", command=saveb, state = tkinter.DISABLED)
     btnsv.grid(column=1, row=0, sticky="NW")
 
-    text = tkinter.Text(window, width=80, height=20)
+    text = tkinter.Text(window, width=80, height=20, state = tkinter.DISABLED)
     text.grid(column=0, row=1, sticky="NEWS")
     scroll = tkinter.Scrollbar(window)
     scroll.config(command=text.yview)
