aa62fb45e4c5414b6f659bca3d309b9cb7340b00
Daniil <chay-chitsd@mail.ru>
2020-06-06 12:54:27 +0300

    Add text

--- pyxxd/__main__.py
+++ pyxxd/__main__.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     window.grid(sticky="NEWS", row=0, column=0)
     window.rowconfigure(0, weight=1)
     window.columnconfigure(0, weight=1)
-    btn = tkinter.Button(window, text="Open", command=clicked)  
+    btn = tkinter.Button(window, text="Open", command=clicked)
     btn.grid(column=0, row=0, sticky="NW")
+    text = tkinter.Text(window, width=100, height=40)
     tkinter.mainloop();
