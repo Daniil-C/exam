cef8f388a04ba408b08b83846d3c97a2ddce718c
Daniil <chay-chitsd@mail.ru>
2020-06-06 15:33:52 +0300

    Fix

--- pyxxd/__main__.py
+++ pyxxd/__main__.py
@@ -11,14 +11,14 @@ FILENMAEOUT = ""
 
 class CustomText(tkinter.Text):
     def __init__(self, *args, **kwargs):
-        tk.Text.__init__(self, *args, **kwargs)
+        tkinter.Text.__init__(self, *args, **kwargs)
         self._orig = self._w + "_orig"
-        self.tk.call("rename", self._w, self._orig)
-        self.tk.createcommand(self._w, self._proxy)
+        self.tkinter.call("rename", self._w, self._orig)
+        self.tkinter.createcommand(self._w, self._proxy)
 
     def _proxy(self, command, *args):
         cmd = (self._orig, command) + args
-        result = self.tk.call(cmd)
+        result = self.tkinter.call(cmd)
 
         if command in ("insert", "delete", "replace"):
             self.event_generate("<<TextModified>>")
