64a6a6fbdf8434ea441037911663f4c01936d247
Daniil <chay-chitsd@mail.ru>
2020-06-06 12:33:46 +0300

    Mv tabs to spaces

--- pyxxd/__main__.py
+++ pyxxd/__main__.py
@@ -5,17 +5,17 @@ FILENAME = ""
 
 def clicked():  
     Tk().withdraw()
-	FILENAME = askopenfilename()
+    FILENAME = askopenfilename()
 
 
 if __name__ == "__main__":
-	window = Frame()
-	window.master.rowconfigure(0, weight=1)
-	window.master.columnconfigure(0, weight=1)
-	window.grid(sticky="NEWS", row=0, column=0)
-	window.rowconfigure(0, weight=1)
-	window.columnconfigure(0, weight=1)
-	btn = Button(window, text="Open", command=clicked)  
-	btn.grid(column=0, row=1, sticky=NEWS)
-	print(FILENAME)
-	mainloop();
+    window = Frame()
+    window.master.rowconfigure(0, weight=1)
+    window.master.columnconfigure(0, weight=1)
+    window.grid(sticky="NEWS", row=0, column=0)
+    window.rowconfigure(0, weight=1)
+    window.columnconfigure(0, weight=1)
+    btn = Button(window, text="Open", command=clicked)  
+    btn.grid(column=0, row=1, sticky=NEWS)
+    print(FILENAME)
+    mainloop();
