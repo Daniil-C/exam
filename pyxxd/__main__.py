16144ffa5fdc19e36e06e2745b184269b4dcf5a5
Daniil <chay-chitsd@mail.ru>
2020-06-06 13:16:27 +0300

    Add btn frame

--- pyxxd/__main__.py
+++ pyxxd/__main__.py
@@ -15,15 +15,17 @@ if __name__ == "__main__":
     window = tkinter.Frame()
     window.master.rowconfigure(0, weight=1)
     window.master.columnconfigure(0, weight=1)
-    window.grid(sticky="NEWS", row=0, column=0)
+    window.grid(ow=0, column=0, sticky="NEWS")
     window.rowconfigure(0, weight=1)
     window.columnconfigure(0, weight=1)
 
-    btnop = tkinter.Button(window, text="Open", command=openb)
+    btns = tkinter.Frame(master = window)
+    btns.grid(row=0, column=0, sticky="NEWS")
+    btnop = tkinter.Button(btns, text="Open", command=openb)
     btnop.grid(column=0, row=0, sticky="NW")
 
-    btnsv = tkinter.Button(window, text="Save", command=saveb)
-    btnsv.grid(column=0, row=0, sticky="NW")
+    btnsv = tkinter.Button(btns, text="Save", command=saveb)
+    btnsv.grid(column=1, row=0, sticky="NW")
 
     text = tkinter.Text(window, width=80, height=20)
     text.grid(column=0, row=1, sticky="NEWS")
